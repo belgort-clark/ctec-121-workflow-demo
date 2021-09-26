5af6679397a41e3a5fdc358c1ee992947998a090
Bruce Elgort <belgort@clark.edu>
2021-09-26 09:11:46 -0700

    Initial commit

---
+++ template.py
@@ -0,0 +1,20 @@
+"""
+CTEC 121
+Date: <ex: mm/dd/yyyy>
+<your name>
+Name: <assignment/lab name>
+Description: <assignment/lab description>
+"""
+
+""" IPO template - use to document functions/methods
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+
+def main():
+    # code here
+
+
+main()
